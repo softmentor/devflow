8720b9f9439519765799888408e6b97e70961ac4
softmentor <softmentor@users.noreply.github.com>
2026-03-02 10:53:16 +0000

    deploy: 7656676827ebfae1eabe8ff570da47df00cc8613

--- rustdoc/trait.impl/devflow_core/extension/trait.Extension.js
+++ rustdoc/trait.impl/devflow_core/extension/trait.Extension.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["devflow_core",[]],["devflow_ext_node",[["impl Extension for <a class=\"struct\" href=\"devflow_ext_node/struct.NodeExtension.html\" title=\"struct devflow_ext_node::NodeExtension\">NodeExtension</a>"]]],["devflow_ext_rust",[["impl Extension for <a class=\"struct\" href=\"devflow_ext_rust/struct.RustExtension.html\" title=\"struct devflow_ext_rust::RustExtension\">RustExtension</a>"]]]]);
+    var implementors = Object.fromEntries([["devflow_core",[]],["devflow_ext_node",[["impl <a class=\"trait\" href=\"devflow_core/extension/trait.Extension.html\" title=\"trait devflow_core::extension::Extension\">Extension</a> for <a class=\"struct\" href=\"devflow_ext_node/struct.NodeExtension.html\" title=\"struct devflow_ext_node::NodeExtension\">NodeExtension</a>"]]],["devflow_ext_rust",[["impl Extension for <a class=\"struct\" href=\"devflow_ext_rust/struct.RustExtension.html\" title=\"struct devflow_ext_rust::RustExtension\">RustExtension</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[19,185,185]}
+//{"start":57,"fragment_lengths":[19,312,185]}
